46698495eb4702cc55fa7ad2e65b8f88b326ef38
ludovicfourcroy <ludovicfourcroy8@gmail.com>
2021-04-29 00:32:01 +0200

    style css and media

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   # Add CRUD routing
   # See all restaurants
   # See details about one restaurant
